da1fef45f9e89e192193f097604c792830518437
Peter Hevesi <peter.hevesi@dfki.de>
2017-11-15 16:11:36 +0100

    Implement beacon data reader, and extraction of beacon ids

--- experiment_handler/imu_data_reader.py
+++ experiment_handler/imu_data_reader.py
@@ -2,6 +2,7 @@ import os
 import json
 import numpy as np
 from experiment_handler.time_synchronisation import convert_timestamps
+from experiment_handler.finder import find_all_imu_files
 
 
 def load_imu_file(filepath):
@@ -15,10 +16,94 @@ def load_imu_file(filepath):
     return lines
 
 
-def get_ble_data(experiment_path, source, start=None, end=None, reference_time=None, convert_time=True):
-    # TODO: add description how the bluetooth beacon data is accessed
+def _get_beacon_id(ble_data, use_uuid=True):
+    if use_uuid:
+        return ble_data['uuid'] + "-" + str(ble_data['major']) + "-" + str(ble_data['minor'])
+    else:
+        return ble_data['macAdd']
 
-    parsed_data = np.zeros((0, 17))
+
+def find_and_categorize_beacon_ids(experiments, threshold=45, save_ids=True):
+    # Recompute set of ids from bluetooth beacons which are apparently static
+    ids_detection_counts = {}
+
+    for exp_root in experiments:
+        imu_files = find_all_imu_files(exp_root)
+        for filepath in imu_files:
+            imu_lines = load_imu_file(filepath)
+
+            for imu_line in imu_lines:
+                try:
+                    data = json.loads(imu_line)
+                except json.decoder.JSONDecodeError:
+                    continue
+
+                if data['type'] != 'ble':
+                    continue
+
+                for beacon in data['beacons']:
+                    current_id = _get_beacon_id(beacon)
+
+                    if current_id in ids_detection_counts.keys():
+                        ids_detection_counts[current_id] += 1
+                    else:
+                        ids_detection_counts[current_id] = 1
+
+    static_ids = []
+    changing_ids = []
+
+    for key in ids_detection_counts.keys():
+        if ids_detection_counts[key] > threshold:
+            static_ids.append(key)
+        else:
+            changing_ids.append(key)
+
+    # save ids into file
+    if save_ids:
+        sorted_ids = {
+            "static_ids": static_ids,
+            "changing_ids": changing_ids
+        }
+        with open('beacon_ids.json', 'w') as fp:
+            json.dump(sorted_ids, fp)
+
+    return static_ids, changing_ids, ids_detection_counts
+
+
+def load_beacon_ids():
+    with open('beacon_ids.json', 'r') as fp:
+        sorted_ids = json.load(fp)
+        return sorted_ids
+
+
+def get_ble_data(experiment_path, source, start=None, end=None, reference_time=None, convert_time=True):
+    """
+    Read beacon data for a given source (e.g. P3 left hand) in a time interval
+
+    Parameters
+    ----------
+    experiment_path: str
+        Root of the experiment (e.g. /data/igroups/experiment_8)
+    source: str
+        Name of the IMU file without extension e.g. P3_imu_right
+    start: float
+        Return values from this timestamp (if reference_time is set, the value is interpreted as time on that channel)
+    end: float
+        Return values until this timestamp (if reference_time is set, the value is interpreted as time on that channel)
+    reference_time: str
+        Use this signal channel's time for reference (convert start and end values to correspond with IMU time)
+    convert_time: bool
+        If set the returned array will contain timestamp in reference_time's values
+
+    Returns
+    -------
+        parsed_data_rssi: numpy array
+            beacon's rssi data with columns order:  <timestamp>, <rssi value of unique id 0>, ...
+        parsed_data_tx: numpy array
+            beacon's tx data with columns order:  <timestamp>, <tx value of unique id 0>, ...
+        unique_ids: list of strings
+            Containing unique beacon ids in the same order as the columns above (offseted by timestamp)
+    """
 
     filepath = os.path.join(experiment_path, "imu", source + ".log")
     imu_lines = load_imu_file(filepath)
@@ -30,33 +115,50 @@ def get_ble_data(experiment_path, source, start=None, end=None, reference_time=N
     if end is not None:
         end_timestamp = convert_timestamps(experiment_path, end, reference_time, imu_reference_time)
 
-        # Parse lines:
-        for imu_line in imu_lines:
-            try:
-                data = json.loads(imu_line)
-            except json.decoder.JSONDecodeError:
-                continue
+    ids = load_beacon_ids()
+    unique_ids = ids['static_ids']
+    parsed_data_tx = np.zeros((0, len(unique_ids) + 1))
+    parsed_data_rssi = np.zeros((0, len(unique_ids) + 1))
 
-            if data['type'] != 'ble':
-                continue
-
-            if start is not None and start_timestamp > data['time']:
-                continue
-            if end is not None and end_timestamp < data['time']:
-                break
-
-            # TODO: parse BLE data
-            current_data = np.zeros((1, 17))
-            print(data)
+    # Parse lines:
+    for imu_line in imu_lines:
+        try:
+            data = json.loads(imu_line)
+        except json.decoder.JSONDecodeError:
             continue
 
-            parsed_data = np.append(parsed_data, current_data, axis=0)
+        if data['type'] != 'ble':
+            continue
 
-        if convert_time:
-            parsed_data[:, 0] = convert_timestamps(experiment_path, parsed_data[:, 0], imu_reference_time,
-                                                   reference_time)
+        new_data_entry_tx = np.zeros((1, len(unique_ids) + 1))
+        new_data_entry_rssi = np.zeros((1, len(unique_ids) + 1))
 
-        return parsed_data
+        new_data_entry_rssi[0, 0] = data['time']
+        new_data_entry_tx[0, 0] = data['time']
+
+        for beacon in data['beacons']:
+            current_id = _get_beacon_id(beacon)
+            if current_id in unique_ids:
+                column_index = unique_ids.index(current_id)
+                new_data_entry_rssi[0, column_index + 1] = beacon['rssi']
+                new_data_entry_tx[0, column_index + 1] = beacon['tx']
+        parsed_data_tx = np.append(parsed_data_tx, new_data_entry_tx, axis=0)
+        parsed_data_rssi = np.append(parsed_data_rssi, new_data_entry_rssi, axis=0)
+
+    if start is not None:
+        parsed_data_tx = parsed_data_tx[parsed_data_tx[:, 0] >= start_timestamp, :]
+        parsed_data_rssi = parsed_data_rssi[parsed_data_rssi[:, 0] >= start_timestamp, :]
+    if end is not None:
+        parsed_data_tx = parsed_data_tx[parsed_data_tx[:, 0] <= end_timestamp, :]
+        parsed_data_rssi = parsed_data_rssi[parsed_data_rssi[:, 0] <= end_timestamp, :]
+
+    if convert_time:
+        parsed_data_rssi[:, 0] = convert_timestamps(experiment_path, parsed_data_rssi[:, 0], imu_reference_time,
+                                               reference_time)
+        parsed_data_tx[:, 0] = convert_timestamps(experiment_path, parsed_data_tx[:, 0], imu_reference_time,
+                                               reference_time)
+
+    return parsed_data_rssi, parsed_data_tx, unique_ids
 
 
 
@@ -157,7 +259,25 @@ def create_imu_log_file_movement_data(log_file_path, npy_file_path):
 
 
 if __name__ == '__main__':
-    exp_root = "/Volumes/DataDrive/igroups_recordings/igroups_experiment_8"
+    if False:
+        experiments = [
+            "/Users/hevesi/ownCloud/Datasets/igroups_experiment_8",
+            "/Users/hevesi/ownCloud/Datasets/igroups_experiment_9"
+        ]
+        good, bad, id_det_counts = find_and_categorize_beacon_ids(experiments)
+        print(good)
+        print("Bad:")
+        print(bad)
 
-    #data = get_ble_data(exp_root, "P3_imu_left", start=652, end=668, reference_time="video", convert_time=True)
-    data = get_imu_data(exp_root, "P3_imu_left", start=652, end=668, reference_time="video", convert_time=True)
+    exp_root = "/Volumes/DataDrive/igroups_recordings/igroups_experiment_8"
+    exp_root = "/Users/hevesi/ownCloud/Datasets/igroups_experiment_8"
+
+    data = get_ble_data(exp_root, "P2_imu_head", start=1800, end=2100, reference_time="video", convert_time=True)
+    #data = get_imu_data(exp_root, "P3_imu_left", start=1800, end=2368, reference_time="video", convert_time=True)
+    print(data[2])
+
+    import matplotlib.pyplot as plt
+
+    plt.imshow(data[0][:, 1:].T)
+    plt.show()
+
